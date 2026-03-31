733cfa2c0788070370e4ebfed7a0b2b021c27b49
yeftapanggabean <2281016@unai.edu>
2026-03-31 13:07:25 +0700

    fix: redirect route to /users

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Livewire\UserTable;
 use App\Http\Controllers\CityOrderReportController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/users');
 });
 
 Route::get('/users', UserTable::class);
